f5daaf82409e453d0e25d46d1ad2c4b87ca78d58
sahil duhan <sahilduhan_se20b9_12@dtu.ac.in>
2023-01-30 20:21:57 +0530

    git udpate

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React from 'react'
-import Covid from './components/Covid'
+import React from "react";
+import Covid from "./components/Covid";
 // import Loading from './components/Loading';
-import './index.css';
+import "./index.css";
 function App() {
-  let current_datetime = new Date()
+  let current_datetime = new Date();
 
   return (
     <div>
       <h1>COVID-19 TRACKER</h1>
-      <h2>Last Updated on : {current_datetime.toLocaleDateString("en-US", { day: 'numeric' })+ "-"+ current_datetime.toLocaleDateString("en-US", { month: 'short' })+ "-" + current_datetime.toLocaleDateString("en-US", { year: 'numeric' })}</h2>
+      <h2>
+        Last Updated on :{" "}
+        {current_datetime.toLocaleDateString("en-US", { day: "numeric" }) +
+          "-" +
+          current_datetime.toLocaleDateString("en-US", { month: "short" }) +
+          "-" +
+          current_datetime.toLocaleDateString("en-US", { year: "numeric" })}
+      </h2>
       {/* {load ? <Loading/> : null} */}
+
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
       {/* <Loading/> */}
-      <Covid/>
+      <Covid />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
